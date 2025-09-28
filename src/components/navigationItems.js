0a554261ef5b6a98897248ecd100be35fc3c4948
samiassasiYA <samii.92@hotmail.com>
2025-09-28 10:56:49 +0300

    remove gallery

--- src/components/navigationItems.js
+++ src/components/navigationItems.js
@@ -9,7 +9,6 @@ const navigationItems = ({ open }) => {
       bg-white shadow-xl w-full z-20 block lg:flex lg:items-center lg:w-auto absolute lg:relative top-full transition duration-500 ease-in-out p-4 pt-0 lg:p-0 lg:shadow-none lg:bg-transparent left-0`}
     >
       <NavigationItem title="Etusivu" slug="/" />
-      <NavigationItem title="Galleria" slug="/galleria" />
       <NavigationItem title="Tuotesarjat" slug="/tuotesarjat" />
     </div>
   );
